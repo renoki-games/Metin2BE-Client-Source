2b3d489d11d2588aa2de49e33bfea9bcb4aeb02c
Lead0b110010100 <Lead0b110010100@outlook.com>
2020-04-12 07:06:58 +0200

    initial commit

---
+++ Extern/include/granny2/granny.h
@@ -0,0 +1,7 @@
+#pragma once
+#define BUILDING_GRANNY_STATIC
+// #include "granny2.4.0.10.h"
+// #include "granny2.7.0.30.h"
+// #include "granny2.8.49.0.h"
+// #include "granny2.9.12.0.h"
+#include "granny2.11.8.0.h"
